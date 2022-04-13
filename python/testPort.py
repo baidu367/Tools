8afb4a88a0144000a221c566fc0bf1488867fb70
zengxingming <zqh0452@163.com>
2022-04-13 15:23:22 +0800

    测试端口是否被占用

---
+++ python/testPort.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# Time : 2021/12/27 17:23 
+# FileName : testPort.py
+# Author : Gecko
+# Description：测试端口是否被占用
+import socket
+
+
+def net_is_used(port, ip='127.0.0.1'):
+    '''
+    :param port: 端口号
+    :param ip: 访问地址
+    :return: True 或 False
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip, port))
+        s.shutdown(2)
+        print('%s:%d is used' % (ip, port))
+        return True
+    except:
+        print('%s:%d is unused' % (ip, port))
+        return False
