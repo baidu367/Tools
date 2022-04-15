0894d3be6856d9d6d4a3803a0942c622a41bed80
zengxingming <zqh0452@163.com>
2022-04-15 11:23:57 +0800

    蚂蚁森林物理工具

--- python/mayisenlin.py
+++ python/mayisenlin.py
@@ -18,14 +18,15 @@ def collectEnergy(cnt):
             if cnt != 1:
                 drvice.click(536, 1816)
 
+
 def start_task():
     cnt = 1
     while True:
         collectEnergy(cnt)
-        a = drvice.xpath("//*[@resource-id='J_tree_dialog_wrap']").get().bounds
         drvice.click(0.858, 0.678)  # 找能量按钮的坐标
         ## 如果页面出现了“返回我的森林”说明已经没有能量可偷了，结束
         if drvice.xpath('//*[@text="返回我的森林"]').click_exists(timeout=2.0):
+            print('能量已收集完成')
             break
         cnt += 1
     print("###结束###")
@@ -33,8 +34,8 @@ def start_task():
 
 
 if __name__ == '__main__':
-    # drvice = u2.connect()  # 有线连接，手机需要插电脑上
-    drvice = u2.connect("192.168.12.173")  # 通过无线连接，电脑和手机需要在同一个局域网内，并且需要先用有线的方式做过初始化
+    drvice = u2.connect_usb()  # 有线连接，手机需要插电脑上
+    # drvice = u2.connect_wifi("192.168.12.173")  # 通过无线连接，电脑和手机需要在同一个局域网内，并且需要先用有线的方式做过初始化
     drvice.app_stop("com.eg.android.AlipayGphone")
     print("打开支付宝")
     drvice.app_start("com.eg.android.AlipayGphone")
