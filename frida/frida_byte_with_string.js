f616ff4a5c36a30db3be3298b5c06e096c7cff6b
zengxingming <zqh0452@163.com>
2022-06-29 12:19:03 +0800

    frida调用函数， 字节与字符串之间相互转换

---
+++ frida/frida_byte_with_string.js
@@ -0,0 +1,251 @@
+// frida调用函数， 字节与字符串之间相互转换
+
+
+// =============================================将byte[]转成String的方法==================================================
+function byteToString(arr) {
+    if (typeof arr === 'string') {
+        return arr;
+    }
+    var str = '',
+        _arr = arr;
+    for (var i = 0; i < _arr.length; i++) {
+        var one = _arr[i].toString(2),
+            v = one.match(/^1+?(?=0)/);
+        if (v && one.length == 8) {
+            var bytesLength = v[0].length;
+            var store = _arr[i].toString(2).slice(7 - bytesLength);
+            for (var st = 1; st < bytesLength; st++) {
+                store += _arr[st + i].toString(2).slice(2);
+            }
+            str += String.fromCharCode(parseInt(store, 2));
+            i += bytesLength - 1;
+        } else {
+            str += String.fromCharCode(_arr[i]);
+        }
+    }
+    return str;
+};
+
+// ==============================================将sting转成byte[]的方法==================================================
+function stringToByte(str) {
+    var bytes = new Array();
+    var len, c;
+    len = str.length;
+    for (var i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if (c >= 0x010000 && c <= 0x10FFFF) {
+            bytes.push(((c >> 18) & 0x07) | 0xF0);
+            bytes.push(((c >> 12) & 0x3F) | 0x80);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+        } else if (c >= 0x000800 && c <= 0x00FFFF) {
+            bytes.push(((c >> 12) & 0x0F) | 0xE0);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+        } else if (c >= 0x000080 && c <= 0x0007FF) {
+            bytes.push(((c >> 6) & 0x1F) | 0xC0);
+            bytes.push((c & 0x3F) | 0x80);
+        } else {
+            bytes.push(c & 0xFF);
+        }
+    }
+    return bytes;
+}
+
+// ==============================================将sting转成base64的方法=====================================================================
+function stringToBase64(e) {
+    var base64EncodeChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    var r, a, c, h, o, t;
+    for (c = e.length, a = 0, r = ''; a < c;) {
+        if (h = 255 & e.charCodeAt(a++), a == c) {
+            r += base64EncodeChars.charAt(h >> 2),
+                r += base64EncodeChars.charAt((3 & h) << 4),
+                r += '==';
+            break
+        }
+        if (o = e.charCodeAt(a++), a == c) {
+            r += base64EncodeChars.charAt(h >> 2),
+                r += base64EncodeChars.charAt((3 & h) << 4 | (240 & o) >> 4),
+                r += base64EncodeChars.charAt((15 & o) << 2),
+                r += '=';
+            break
+        }
+        t = e.charCodeAt(a++),
+            r += base64EncodeChars.charAt(h >> 2),
+            r += base64EncodeChars.charAt((3 & h) << 4 | (240 & o) >> 4),
+            r += base64EncodeChars.charAt((15 & o) << 2 | (192 & t) >> 6),
+            r += base64EncodeChars.charAt(63 & t)
+    }
+    return r
+}
+
+// ==============================================将base64转成string的方法=====================================================================
+function base64ToString(e) {
+    var base64DecodeChars = new Array((-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), 62, (-1), (-1), (-1), 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, (-1), (-1), (-1), (-1), (-1), (-1), (-1), 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, (-1), (-1), (-1), (-1), (-1), (-1), 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, (-1), (-1), (-1), (-1), (-1));
+    var r, a, c, h, o, t, d;
+    for (t = e.length, o = 0, d = ''; o < t;) {
+        do
+            r = base64DecodeChars[255 & e.charCodeAt(o++)];
+        while (o < t && r == -1);
+        if (r == -1)
+            break;
+        do
+            a = base64DecodeChars[255 & e.charCodeAt(o++)];
+        while (o < t && a == -1);
+        if (a == -1)
+            break;
+        d += String.fromCharCode(r << 2 | (48 & a) >> 4);
+        do {
+            if (c = 255 & e.charCodeAt(o++), 61 == c)
+                return d;
+            c = base64DecodeChars[c]
+        } while (o < t && c == -1);
+        if (c == -1)
+            break;
+        d += String.fromCharCode((15 & a) << 4 | (60 & c) >> 2);
+        do {
+            if (h = 255 & e.charCodeAt(o++), 61 == h)
+                return d;
+            h = base64DecodeChars[h]
+        } while (o < t && h == -1);
+        if (h == -1)
+            break;
+        d += String.fromCharCode((3 & c) << 6 | h)
+    }
+    return d
+}
+
+// ==============================================将十六进制转成base64的方法======================================================================
+function hexToBase64(str) {
+    return base64Encode(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+}
+
+// ==============================================将base64转成十六进制的方法=====================================================================
+function base64ToHex(str) {
+    for (var i = 0, bin = base64Decode(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
+        var tmp = bin.charCodeAt(i).toString(16);
+        if (tmp.length === 1)
+            tmp = "0" + tmp;
+        hex[hex.length] = tmp;
+    }
+    return hex.join("");
+}
+
+// ==============================================将十六进制转成byte[]的方法======================================================================
+function hexToBytes(str) {
+    var pos = 0;
+    var len = str.length;
+    if (len % 2 != 0) {
+        return null;
+    }
+    len /= 2;
+    var hexA = new Array();
+    for (var i = 0; i < len; i++) {
+        var s = str.substr(pos, 2);
+        var v = parseInt(s, 16);
+        hexA.push(v);
+        pos += 2;
+    }
+    return hexA;
+}
+
+// ==============================================将string转成十六进制的方法======================================================================
+function stringToHex(str) {
+    var val = "";
+    for (var i = 0; i < str.length; i++) {
+        if (val == "")
+            val = str.charCodeAt(i).toString(16);
+        else
+            val += str.charCodeAt(i).toString(16);
+    }
+    return val
+}
+
+// ===============================================将sting转成byte[]的方法=====================================================================
+var stringToBytes = function (str) {
+    var ch, st, re = [];
+    for (var i = 0; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        st = [];
+        do {
+            st.push(ch & 0xFF);
+            ch = ch >> 8;
+        }
+        while (ch);
+        re = re.concat(st.reverse());
+    }
+    return re;
+}
+
+// ===============================================将byte[]转成String的方法=====================================================================
+var bytesToString = function (arr) {
+    var str = '';
+    arr = new Uint8Array(arr);
+    for (i in arr) {
+        str += String.fromCharCode(arr[i]);
+    }
+    return str;
+}
+
+// ===============================================将byte[]转成base64的方法=====================================================================
+var bytesToBase64 = function (e) {
+    var base64EncodeChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    var r, a, c, h, o, t;
+    for (c = e.length, a = 0, r = ''; a < c;) {
+        if (h = 255 & e[a++], a == c) {
+            r += base64EncodeChars.charAt(h >> 2),
+                r += base64EncodeChars.charAt((3 & h) << 4),
+                r += '==';
+            break
+        }
+        if (o = e[a++], a == c) {
+            r += base64EncodeChars.charAt(h >> 2),
+                r += base64EncodeChars.charAt((3 & h) << 4 | (240 & o) >> 4),
+                r += base64EncodeChars.charAt((15 & o) << 2),
+                r += '=';
+            break
+        }
+        t = e[a++],
+            r += base64EncodeChars.charAt(h >> 2),
+            r += base64EncodeChars.charAt((3 & h) << 4 | (240 & o) >> 4),
+            r += base64EncodeChars.charAt((15 & o) << 2 | (192 & t) >> 6),
+            r += base64EncodeChars.charAt(63 & t)
+    }
+    return r
+}
+
+// ===============================================将base64转成byte[]的方法=====================================================================
+var base64ToBytes = function (e) {
+    var base64DecodeChars = new Array((-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), (-1), 62, (-1), (-1), (-1), 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, (-1), (-1), (-1), (-1), (-1), (-1), (-1), 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, (-1), (-1), (-1), (-1), (-1), (-1), 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, (-1), (-1), (-1), (-1), (-1));
+    var r, a, c, h, o, t, d;
+    for (t = e.length, o = 0, d = []; o < t;) {
+        do
+            r = base64DecodeChars[255 & e.charCodeAt(o++)];
+        while (o < t && r == -1);
+        if (r == -1)
+            break;
+        do
+            a = base64DecodeChars[255 & e.charCodeAt(o++)];
+        while (o < t && a == -1);
+        if (a == -1)
+            break;
+        d.push(r << 2 | (48 & a) >> 4);
+        do {
+            if (c = 255 & e.charCodeAt(o++), 61 == c)
+                return d;
+            c = base64DecodeChars[c]
+        } while (o < t && c == -1);
+        if (c == -1)
+            break;
+        d.push((15 & a) << 4 | (60 & c) >> 2);
+        do {
+            if (h = 255 & e.charCodeAt(o++), 61 == h)
+                return d;
+            h = base64DecodeChars[h]
+        } while (o < t && h == -1);
+        if (h == -1)
+            break;
+        d.push((3 & c) << 6 | h)
+    }
+    return d
+}
